1e73e377f13e8da0a76ca6ce16a879ec335e222a
akhodas <a.l.khodas@gmail.com>
2019-05-05 21:09:54 +0300

    added prop-types and created component place-card

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 import {Stateless} from "../stateless/stateless.jsx";
 
-export const App = () => {
-  return <Stateless />;
+export const App = (props) => {
+  const {namesPlaceCard} = props;
+
+  return <Stateless
+    namesPlace = {namesPlaceCard}
+  />;
+};
+
+App.propTypes = {
+  namesPlaceCard: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from "react-dom";
 import {App} from "./components/app/app.jsx";
 
 const init = () => {
+  const settings = {
+    namesPlaceCard: [
+      `Beautiful & luxurious apartment at great location`,
+      `Wood and stone place`,
+      `Canal View Prinsengracht`,
+      `Nice, cozy, warm big bed apartment`,
+    ],
+  };
+
   ReactDOM.render(
-      <App/>,
+      <App
+        namesPlaceCard = {settings.namesPlaceCard}
+      />,
       document.querySelector(`#root`)
   );
 };
