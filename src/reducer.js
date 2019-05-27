0493c783a7813144a779b14a90fbfd2d4d85b971
akhodas <a.l.khodas@gmail.com>
2019-05-28 00:30:48 +0300

    install redux and react-redax, add reducer

---
+++ src/reducer.js
@@ -0,0 +1,37 @@
+const initialState = {
+  city: `Minsk`,
+  listOffers: [],
+};
+
+const ActionCreators = {
+  changeCity: (newCity) => ({
+    type: `CHANGE_CITY`,
+    payload: newCity,
+  }),
+
+  addListOffers: (list) => ({
+    type: `ADD_LIST_OFFERS`,
+    payload: list,
+  }),
+};
+
+const reducer = (state = initialState, action) =>{
+  switch (action.type) {
+    case `CHANGE_CITY`:
+      return Object.assign({}, state, {
+        city: action.payload,
+      });
+
+    case `ADD_LIST_OFFERS`:
+      return Object.assign({}, state, {
+        listOffers: action.payload,
+      });
+  }
+  return state;
+};
+
+export {
+  ActionCreators,
+  reducer,
+};
+
